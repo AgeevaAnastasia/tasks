cb70d9b21e3c0d0041513ecc275851bd2f2e1466
Anastasia Ageeva <anastasia.o.ageeva@gmail.com>
2022-07-09 15:15:01 +0300

    Add Task00 finding square

---
+++ Task00_Square/Program.cs
@@ -0,0 +1,9 @@
+﻿// Task00
+//Прорамма на вход получает число N и на выходе выдает квадрат этого числа. 
+// N=2 -> 4     N=-4->16 и так далее
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {result}");
