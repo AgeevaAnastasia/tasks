9317ad1cf110af381a3c2038b1b8a55bc9c601a2
Anastasia Ageeva <anastasia.o.ageeva@gmail.com>
2022-07-15 19:52:31 +0300

    Add Task 012

---
+++ Task012_MultipleFirstToSecond/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход 2 числа и выводит, является ли второе кратным первому. 
+// Если не кратно, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16Б 4 -> кратно
+
+Console.Write("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+int result = first % second;
+if (result == 0) Console.WriteLine($"Число {first} кратно числу {second}");
+else Console.WriteLine($"Число {first} не кратно числу {second}, остаток от деления равен {result}");
+
