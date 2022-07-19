482cc8636da17b17763217933ee0a0d1eaf0b2f4
Anastasia Ageeva <anastasia.o.ageeva@gmail.com>
2022-07-19 23:23:33 +0300

    Add Task 017

--- Task013/Program.cs
+++ Task013/Program.cs
@@ -22,3 +22,28 @@ else
 
     Console.WriteLine($"Третья цифра числа это {res}");
 }
+
+
+// int index = 1, a = 0;
+//  Console.WriteLine("введите число не менее трехзначного");
+        
+//         int num = Convert.ToInt32(Console.ReadLine());
+//          if (num>=100) // если число 3х значное то...
+//          { while (index <= num)        //определяем  из скольки чисел состоит число  и передаем в переменную а
+//         {
+//             a = a + 1;
+//             index = index * 10;
+//         }   
+               
+//         int[] nums = new int[a];         // обьявлем массив размером в количество символов введеннного числа
+//         int indexArray = nums.Length-1;
+//         while (indexArray>=0)
+//         {
+//             nums [indexArray] = num %  10;         
+//             num = num/10;
+//             indexArray=indexArray-1;
+           
+//         }
+//         Console.WriteLine($"третья цифра введенного числа составляет = {nums[2]} ");}
+//          else Console.WriteLine("у Введенного числа нет 3 цифры");
+         
