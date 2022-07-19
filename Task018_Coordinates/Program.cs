f009d069065ad090463977b31824e69fb5574b4b
Anastasia Ageeva <anastasia.o.ageeva@gmail.com>
2022-07-19 23:50:33 +0300

    Add Task 018

---
+++ Task018_Coordinates/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти (x, y)
+
+Console.Write("Введите номер четверти на координатной плоскости (1, 2, 3 или 4): ");
+string num = Console.ReadLine();
+
+string Coordinates(string numQ)
+{
+    if (numQ == "1") return "x положительный, y положительный";
+    if (numQ == "2") return "x отрицательный, y положительный";
+    if (numQ == "3") return "x отрицательный, y отрицательный";
+    if (numQ == "4") return "x положительный, y отрицательный";
+    return "Введен некорректный номер четверти";
+}
+
+string result = Coordinates(num);
+Console.WriteLine(result);
