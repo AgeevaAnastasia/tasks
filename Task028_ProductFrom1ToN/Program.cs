b67086fef247e906551e6b21bc9796af5f573ef7
Anastasia Ageeva <anastasia.o.ageeva@gmail.com>
2022-08-03 19:07:49 +0300

    Add Task028 Product From 1 To N

---
+++ Task028_ProductFrom1ToN/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число n и выдает произведение чисел от 1 до n
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число: ");
+int n = Int32.Parse(Console.ReadLine());
+
+int GetProduct(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum * i;   
+    }
+    return sum;
+}
+
+int res = GetProduct(n);
+Console.WriteLine($"Произведение чисел от 1 до {n} равно {res}");
