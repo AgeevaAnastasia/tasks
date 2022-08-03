409916e168a055fee24600096ff8f91793d7f87a
Anastasia Ageeva <anastasia.o.ageeva@gmail.com>
2022-08-03 10:58:21 +0300

    Add Task 024 Sum from 1 to Т

---
+++ Task024_Sum1ToN/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает на выход сумму чисел от 1 до N
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите целое положительное число");
+int n = Int32.Parse(Console.ReadLine());
+if (n > 0)
+{
+    int sum = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+    Console.WriteLine($"{n} -> {sum}");
+}
+else Console.WriteLine("Вы ввели нерелевантное число");
