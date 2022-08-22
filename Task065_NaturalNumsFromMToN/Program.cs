61a056a73989237850d1a246b3986adb9ac96077
Anastasia Ageeva <anastasia.o.ageeva@gmail.com>
2022-08-22 17:54:02 +0300

    Add Task 067 RecursionSumOfDigits

--- Task065_NaturalNumsFromMToN/Program.cs
+++ Task065_NaturalNumsFromMToN/Program.cs
@@ -3,19 +3,66 @@
 // M = 4; N = 8 -> "4, 6, 7, 8"
 
 Console.Clear();
-Console.Write("Введите натуральное число M (меньшее): ");
+Console.Write("Введите натуральное число M: ");
 int mUser = Int32.Parse(Console.ReadLine());
-Console.Write("Введите натуральное число N (большее): ");
+Console.Write("Введите натуральное число N: ");
 int nUser = Int32.Parse(Console.ReadLine());
 
-Console.Clear();
-Console.Write($"M = {mUser}, N = {nUser} -> ");
-NaturalNumbersMtoN (mUser, nUser);
+// Console.Clear();
+// if (mUser < nUser)
+// {
+//     Console.Write($"M = {mUser}, N = {nUser} -> ");
+//     NaturalNumbersMtoN(mUser, nUser);
+// }
+// else
+// {
+//     Console.Write($"N = {nUser}, M = {mUser} -> ");
+//     NaturalNumbersNtoM (mUser, nUser);
+// }
+// Console.WriteLine();
+
+// // void NaturalNumbersMtoN (int m, int n)
+// // {
+// //     if (n < m) return;
+// //     NaturalNumbersMtoN(m, n - 1);
+// //     Console.Write($"{n} ");
+// // }
+
+// void NaturalNumbersMtoN(int m, int n)
+// {
+//     if (n < m) return;
+//     Console.Write($"{m} ");
+//     NaturalNumbersMtoN(++m, n);
+// }
+
+// void NaturalNumbersNtoM(int m, int n)
+// {
+//     if (n > m) return;
+//     Console.Write($"{n} ");
+//     NaturalNumbersNtoM(m, ++n);
+// }
+
+GetNaturalNums(mUser, nUser);
 Console.WriteLine();
 
-void NaturalNumbersMtoN (int m, int n)
+void GetNaturalNums (int m, int n)
 {
-    if (n == m - 1) return;
-    NaturalNumbersMtoN(m, n - 1);
-    Console.Write($"{n} ");
+	if (n == m)
+    {
+        Console.Write($"{m} ");
+        return;
+    }
+
+    if (m > n)
+    {
+        Console.Write($"{m} ");
+        GetNaturalNums(m - 1, n);
+    }
+
+    else
+    {
+        GetNaturalNums(m, n - 1);
+        Console.Write($"{n} ");
+    }
+
 }
---
+++ Task067_RecursionSumOfDigits/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Clear();
+int numUser = InputNumbers("Введите число: ")
+
+Console.Write($"{numUser} -> ");
+int res = GetSumOfDigits(numUser);
+Console.Write(res);
+Console.WriteLine();
+
+int GetSumOfDigits(int num)
+{
+    int sum = 0;
+    if (num == 0) return sum;
+    GetSumOfDigits(num / 10);
+    sum = sum + numUser % 10;
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
